0129f19db13c3298a79c5915b6e50f8f670b915c
valentina-19 <68057012+valentina-19@users.noreply.github.com>
2022-07-15 20:55:27 +0300

    Update Account.java

--- Account.java
+++ Account.java
@@ -2,6 +2,8 @@ package practice_5_baby_blockchain;
 
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Set;
 
 public class Account {
 
@@ -23,24 +25,53 @@ public class Account {
 	public ArrayList<KeyPair> getWallet() {
 		return wallet;
 	}
+	
+	// функция генерации уникальной ключевой пары
+	public static KeyPair genUniqueKeyPair (Blockchain blockchain) {
+		boolean isUnique;	
+		KeyPair keypair;
+		ArrayList<KeyPair> wallet;
+		int balance = 0;
+		do {
+			isUnique = false;	
+			keypair = KeyPair.genKeyPair(blockchain.getRnd());
+			wallet = new ArrayList<KeyPair>();
+			Account account;
+			KeyPair keyPairTemp;
+			Iterator<Account> accountIterator = blockchain.getCoinDataBase().keySet().iterator();
+			for (int i = 0; i < blockchain.getCoinDataBase().keySet().size(); i++) {
+				account = accountIterator.next();
+						
+				for (int j = 0; j < account.getWallet().size(); j++) {
+					keyPairTemp = account.getWallet().get(j);
+					if (keyPairTemp.getPublicKey().toString().compareTo(keypair.getPublicKey().toString()) == 0) {
+						isUnique = true;
+						break;
+					}
+				}
+				if (isUnique) {
+					break;
+				}
+			}		
+				
+		} while(isUnique);
+		
+		return keypair;
 
-	public static Account genAccount () {
-		KeyPair keypair = KeyPair.genKeyPair();
+	}
+
+	// функция позволяющая выполнить создание аккаунта.
+	public static Account genAccount (Blockchain blockchain) throws Exception {
+		KeyPair keypair = genUniqueKeyPair (blockchain);
 		String accountID = keypair.getPublicKey().toString();
-		Blockchain blockchain = new Blockchain();
-		if(blockchain.isContainAccountID(accountID)) {
-			return null;
-		} else {
 		ArrayList<KeyPair> wallet = new ArrayList<KeyPair>();
-		if (!wallet.contains(keypair)) {
-			wallet.add(keypair);
-		}
-		int balance = 0;	
+		int balance = 0;
+		wallet.add(keypair);
 		return new Account(accountID, wallet, balance);	
-		}
 			
 	}
 	
+	// функция позволяющая добавить в кошелек новую ключевую пару
 	public void addKeyPairToWallet (KeyPair keypair) {
 		if (!wallet.contains(keypair)) {
 			wallet.add(keypair);
@@ -48,6 +79,7 @@ public class Account {
 		
 	}
 	
+	// функция позволяющая обновить состояние баланса пользователя.
 	public void updateBalance (int balance) throws UnsupportedEncodingException {
 		this.balance = balance;
 	}
@@ -56,10 +88,12 @@ public class Account {
 		return balance;
 	}
 	
+	// вывод баланса аккаунта на экран
 	public void printBalance() {
 		System.out.println(getBalance());
 	}
 	
+	// функция позволяющая подписать произвольные данные пользователем
 	public byte [] signData (String message, int index) throws UnsupportedEncodingException {
 		
 		return Signature.signData(wallet.get(index).getPrivateKey(), message);
@@ -72,7 +106,8 @@ public class Account {
 		return "Account [accountID=" + accountID + ", wallet=" + wallet + ", balance=" + balance + "]";
 	}
 
-	public Operation createPaymentOp (Account recipient, int amount, int index) throws UnsupportedEncodingException {
+	// функция которая позволяет создать операцию платежа от имени этого аккаунта получателю
+	public Operation createPaymentOp (Account recipient, int amount, int index) throws Exception {
 		
 		Operation resOperation = Operation.createOperation(this, recipient, amount);
 		resOperation.setSignature(signData(resOperation.toString(), index));
